95625584572aa8d013c771726f76a5025fabce8d
nicolicioiu liviu <liviu.nicolicioiu@epoint.ro>
2018-01-17 16:08:53 +0200

    Add dev bash script

---
+++ dev.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -euo pipefail # https://coderwall.com/p/fkfaqq/safer-bash-scripts-with-set-euxo-pipefail
+
+COMPOSE="docker-compose"
+EXEC="docker exec -i"
+EXEC_BIN="bash"
+EXEC_MVN="mvn"
+PROJECT_HOSTNAME="localhost:8080"
+TEST_ENVIRONMENT="local"
+if [ $# -gt 0 ];then
+    if [ "$1" == "start" ]; then
+        $COMPOSE start
+
+    elif [ "$1" == "stop" ]; then
+        $COMPOSE stop
+
+    elif [ "$1" == "clean" ]; then
+        echo "Clean";
+    elif [ "$1" == "build" ]; then
+        $EXEC_MVN package
+
+    elif [ "$1" == "system-integration" ]; then
+        cd system-integration
+        ./ht/ht_linux exec -output ./reports/ -Dfile ./env/local.json ./entity/...
+    elif [ "$1" == "run" ]; then
+        ./dev.sh build
+        java -jar target/entityAPI-1.0-SNAPSHOT.jar &
+        echo "Started on $PROJECT_HOSTNAME"
+    else
+        $COMPOSE "$@"
+    fi
+else
+    $COMPOSE ps
+fi
